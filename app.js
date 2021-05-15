cdc8b5a5b80a77d973eda5195670475d09d6fc32
Sarah Loutrel <s.loutrel@gmail.com>
2021-05-14 17:21:50 -0700

    path updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const magic = document.querySelector('.mtg');
 magic.addEventListener('click', function () {
-    window.open('https://sloutrel.github.io/score', '_blank')
+    window.open('https://sloutrel.github.io/magic', '_blank')
 })
 
