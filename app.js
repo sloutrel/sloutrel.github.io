4fc7b4abbdfcbf4fd843b019a6ae6ac979c51aa1
Sarah Loutrel <s.loutrel@gmail.com>
2021-05-25 21:34:46 -0700

    portfolio updates

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const magic = document.querySelector('.mtg');
-magic.addEventListener('click', function () {
-    window.open('https://sloutrel.github.io/magic', '_blank')
-})
+// const magic = document.querySelector(".mtg");
+// magic.addEventListener("click", function () {
+//   window.open("https://sloutrel.github.io/magic", "_blank");
+// });
 
+const scrolled = () => {
+  document.scroll(function () {
+    const navbar = document.createElement("mainNavbar");
+    navbar.toggleClass("scrolled", this.scrollTop() > navbar.height());
+  });
+};
