518d9ac32956375117679ebc54f2785ac7203a37
Sarah Loutrel <s.loutrel@gmail.com>
2021-09-10 23:27:19 -0700

    update saplants

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-// const magic = document.querySelector(".mtg");
-// magic.addEventListener("click", function () {
-//   window.open("https://sloutrel.github.io/magic", "_blank");
-// });
-
 const scrolled = () => {
   document.scroll(function () {
-    const navbar = document.createElement("mainNavbar");
-    navbar.toggleClass("scrolled", this.scrollTop() > navbar.height());
+    const navbar = document.createElement('mainNavbar');
+    navbar.toggleClass('scrolled', this.scrollTop() > navbar.height());
   });
 };
--- src/cards.js
+++ src/cards.js
@@ -2,27 +2,37 @@
 const AppRender = () => {
   const appData = [
     {
-      id: 0,
+      id: 12,
       alt: 'Animal img',
       title: 'YelpCat - ExpressJS w/Authentication',
-      text: 'Need that 6th cat <em>and,/em> despise kitty mills? Join YelpCat, a Yelp-like clone for pet adoption, today!',
+      text: 'Need that 6th cat AND despise kitty mills? Join YelpCat, a Yelp-like clone for pet adoption!',
       aRef: 'https://secret-tor-03211.herokuapp.com/',
-      button: 'Adopt a furrbaby today!',
+      button: 'Adopt a fur baby today!',
       image:
         'https://images.unsplash.com/photo-1508675801627-066ac4346a61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=745&q=80',
     },
     {
-      id: 1,
+      id: 11,
+      alt: 'Plant img',
+      title: 'Saplants Landing Page - Pure CSS/SASS',
+      text: 'Channel your inner plant daddy and order your box of plants today! No delivery guarantees.',
+      aRef: 'https://sloutrel.github.io/saplants',
+      button: 'For the environment!',
+      image:
+        'https://images.unsplash.com/photo-1599060114289-1f072c1e9b79?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      id: 10,
       alt: 'Magic the gathering img',
       title: 'Magic the Gathering Score Keeper - React',
-      text: "New and enhanced. Fight all the Josh's until only you survive. Still without that pesky 20 sided die.",
+      text: 'New and enhanced. Fight until only you survive. Still without that pesky 20 sided die.',
       aRef: 'https://sloutrel.github.io/magic-react',
       button: 'Enter the battlefield',
       image:
         'https://www.cruiseplanners.com/static/imago/5556/magic_banner-1000x2821.jpg',
     },
     {
-      id: 2,
+      id: 9,
       alt: 'magic the gathering img',
       title: 'Magic the Gathering Score Keeper - VanillaJS',
       text: 'Choose your life count and keep score easily without that pesky 20 sided die.',
@@ -32,27 +42,27 @@ const AppRender = () => {
         'https://seller.tcgplayer.com/media/2439/magic_media_slider_042018-2x.jpg',
     },
     {
-      id: 3,
+      id: 8,
       alt: 'expense-list img',
       title: 'Basic Expense List - React',
-      text: 'Have you spent your hard earned money on too much rent? Is your boba habit getting out of control?',
+      text: 'Have you spent your money on too much rent? Is your boba habit getting out of control?',
       aRef: 'https://sloutrel.github.io/expense-list',
       button: 'See the damage.',
       image:
         'https://images.unsplash.com/photo-1580519542036-c47de6196ba5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80',
     },
     {
-      id: 4,
+      id: 7,
       alt: 'task list img',
       title: 'Task List - VanillaJS / CSS',
-      text: "Is life getting too crazy to keep up? Don't worry about it! Just add it to a list & everything will be okay.",
+      text: "Is life getting too crazy? Don't worry about it! Just add it to a list & everything will be okay.",
       aRef: 'https://sloutrel.github.io/todo',
       button: "Let's get to Listin'",
       image:
         'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80',
     },
     {
-      id: 5,
+      id: 6,
       alt: 'task list img',
       title: 'Task List - React',
       text: 'Have you always wanted to be a Taskmaster? Unleash your inner type-A, micromanger self today!',
@@ -62,37 +72,37 @@ const AppRender = () => {
         'https://images.unsplash.com/photo-1536860484431-7615d914eac1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
     },
     {
-      id: 6,
+      id: 5,
       alt: 'cat img',
       title: 'Museum of Cats - Bootstrap',
-      text: 'Come look at some meowsterpieces at the meowseum.',
+      text: 'Come look at some meow-sterpieces at the meow-seum.',
       aRef: 'https://sloutrel.github.io/museum-of-cats',
       button: 'Meow',
       image:
         'https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80',
     },
     {
-      id: 7,
+      id: 4,
       alt: 'blocks img',
       title: 'Colorful Block Grid - React',
-      text: "It's kinda like a fidgit spinner, but with any number of multi-colored blocks. Click to change the colors.",
+      text: "It's kinda like a fidgit spinner, but with multi-colored blocks. Click to change the colors.",
       aRef: 'https://sloutrel.github.io/colored-blocks',
       button: 'Click at you own risk.',
       image:
         'https://static.vecteezy.com/system/resources/thumbnails/001/857/157/small/abstract-background-of-multi-colored-cubes-vector.jpg',
     },
     {
-      id: 8,
+      id: 3,
       alt: 'coin flip img',
       title: 'Test your luck - flip a cat coin - React',
-      text: "Do you relate to Two Face and let lady luck make your decisions? Are cat's also you entire personality?",
+      text: "Are you like Two Face & lady luck makes your decisions? Are cat's also your entire personality?",
       aRef: 'https://sloutrel.github.io/cat-coin',
       button: 'Do you feel lucky, punk?',
       image:
         'https://www.vertica.com/wp-content/uploads/2019/07/Coin_Flip_183981489-2160.jpg',
     },
     {
-      id: 9,
+      id: 2,
       alt: 'dice img',
       title: 'Two dice - no more, no less - React',
       text: 'Dice are so heeeeaavy! Save your arm from a lifetime of pain with this handy tool.',
@@ -102,7 +112,7 @@ const AppRender = () => {
         'https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGljZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
     },
     {
-      id: 10,
+      id: 1,
       alt: 'random img',
       title: 'Styles Generator - VanillaJS',
       text: "Don't know how to pair styles? Neither does this app. Generate random style combos here.",
